06015d30006e9781ae002920a61613cbfa0ce3c2
Samer taha <alrazy1@gmail.com>
2023-09-28 13:47:28 +0300

    setup initially google auth login by next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import Button from "@/components/ui/Button"
 import { db } from "@/lib/db"
 
 export default async function Home() {
-  return (
-    <div className='text-red-500'>hello world</div>
-  )
+  return <Button variant={"ghost"}>Hello</Button>
 }
---
+++ src/components/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FC, ReactNode } from 'react'
+import { Toaster } from 'react-hot-toast'
+
+interface providersProps {
+    children: ReactNode
+}
+
+const providers: FC<providersProps> = ({children}) => {
+    return <>
+        <Toaster position='top-center' reverseOrder={false} />
+        {children}
+        </>
+}
+
+export default providers
