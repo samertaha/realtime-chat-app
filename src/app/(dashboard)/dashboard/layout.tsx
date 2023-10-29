4fb5b06e203ea7049caebed87f60e98fec5b37c0
Samer taha <alrazy1@gmail.com>
2023-10-29 14:50:41 +0200

    added mobile responsive feature

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -10,16 +10,11 @@ import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOption
 import { fetchRedis } from "@/helpers/redis"
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id"
 import SidebarChatList from "@/components/SidebarChatList"
+import MobileChatLayout from "@/components/MobileChatLayout"
+import { SidebarOption } from "@/types/typings"
 
 interface LayoutProps {
   children: ReactNode
-}
-
-interface SidebarOption {
-  id: number
-  name: string
-  href: string
-  Icon: Icon
 }
 
 const sidebarOptions: SidebarOption[] = [
@@ -46,7 +41,16 @@ const Layout = async ({ children }: LayoutProps) => {
 
   return (
     <div className="w-full flex h-screen">
-      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div className="md:hidden">
+        <MobileChatLayout
+          friends={friends}
+          session={session}
+          sideBarOptions={sidebarOptions}
+          unseenRequestCount={unseenRequestCount}
+        />
+      </div>
+
+      <div className="hidden md:flex flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link
           href="/dashboard"
           className="flex h-16 shrink-0 items-center"
